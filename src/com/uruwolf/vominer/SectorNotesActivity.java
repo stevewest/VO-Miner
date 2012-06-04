88bffaac724e1ad840743614ff85f2afa8b5c4f3
Steve "Uru" West <uruwolf@gmail.com>
2012-06-04 16:08:02 +0100

    close button now closes

--- src/com/uruwolf/vominer/SectorNotesActivity.java
+++ src/com/uruwolf/vominer/SectorNotesActivity.java
@@ -5,9 +5,17 @@ import com.uruwolf.vominer.data.SectorDataSource;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * Shows notes relating to a sector. When called from an intent must have system, alpha and num in the intent extras.
+ * @author Steve "Uru" West <uruwolf@gmail.com>
+ * 
+ */
 public class SectorNotesActivity extends Activity{
 	
 	private String system;
@@ -16,21 +24,39 @@ public class SectorNotesActivity extends Activity{
 	private SectorDataSource data;
 	private Sector sector;
 	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.sector_notes);
         
+        //Make sure we can use the database
         data = new SectorDataSource(this);
         data.open();
         
+        //Grab the extras so we know what sector we are dealing with
         Bundle extras = getIntent().getExtras();
         system = extras.getString("system");
         alpha = extras.getString("alpha");
         num = extras.getString("num");
         
+        //Set the sector title
         String title = String.format(getResources().getString(R.string.sector_full_title), system, alpha, num);
-        
         ((TextView) findViewById(R.id.sectorTitle)).setText(title);
+        
+        //Make sure the close button closes
+        ((Button)findViewById(R.id.closeButton)).setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				shutdown();
+			}
+        });
+	}
+	
+	/**
+	 * Closes the activity
+	 */
+	private void shutdown(){
+		this.finish();
 	}
 	
 	@Override
